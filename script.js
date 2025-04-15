257d89dc43b6c71340ff9a9d28d11be646b8a3f4
Laura <laura.makila81@gmail.com>
2025-04-15 08:43:42 +0300

    hampurilaisvalikon responsiivisuus asettelut kuntoon

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const navbar = document.querySelector(".navbar");
 
-    const mobileMenu = document.getElementById("mobile-menu");
-    const navbar = document.querySelector(".navbar");
+// Hampurilaisvalikon toiminnallisuus
+mobileMenu.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
 
-    mobileMenu.addEventListener("click", () => {
-        navbar.classList.toggle("active");
-    });
+document.querySelectorAll('.navbar a').forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+    }
+});
 
-    document.querySelectorAll('.navbar a').forEach(link => {
-        if (link.href === window.location.href) {
-            link.classList.add('active');
-        }
-    });
+
+
